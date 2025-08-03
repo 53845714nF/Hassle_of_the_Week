eb3fcf14a01177bc866520552160351b658db87b
sebastian <hackwiki2.0@gmail.com>
2025-08-03 21:24:44 +0200

    fix css

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Hassel of the Week
+
+Small presentation in connection with the module Human–computer interaction, Part of the third semester computer science.
+
+Look [here](https://hotw.hackwiki.de/)
--- local.sh
+++ local.sh
@@ -4,15 +4,18 @@
 [ ! -d js ]  && mkdir js
 
 # Check css files if not download
-[ ! -f css/league.css ] && wget https://unpkg.com/reveal.js@^4//dist/theme/league.css -O css/league.css
-[ ! -f css/reset.css ]  && wget https://unpkg.com/reveal.js@^4//dist/reset.css		  -O css/reset.css
-[ ! -f css/reveal.css ] && wget https://unpkg.com/reveal.js@^4//dist/reveal.css		  -O css/reveal.css
+[ ! -f css/moon.css ] && wget https://unpkg.com/reveal.js@4.6.1/dist/theme/moon.css -O css/moon.css
+[ ! -f css/reset.css ]  && wget https://unpkg.com/reveal.js@^4.6.0//dist/reset.css		  -O css/reset.css
+[ ! -f css/reveal.css ] && wget https://unpkg.com/reveal.js@^4.6.0//dist/reveal.css		  -O css/reveal.css
+[ ! -f css/source-sans-pro.css ] && wget https://unpkg.com/reveal.js@4.6.1/dist/theme/fonts/source-sans-pro/source-sans-pro.css -O css/source-sans-pro.css
+
 
 # Check js files if not download
-[ ! -f js/search.js ] && wget https://unpkg.com/reveal.js@^4//plugin/search/search.js -O js/search.js
-[ ! -f js/notes.js ]  && wget https://unpkg.com/reveal.js@^4//plugin/notes/notes.js   -O js/notes.js
-[ ! -f js/reveal.js ] && wget https://unpkg.com/reveal.js@^4//dist/reveal.js		  -O js/reveal.js
-[ ! -f js/zoom.js ]   && wget https://unpkg.com/reveal.js@^4//plugin/zoom/zoom.js	  -O js/zoom.js
+[ ! -f js/search.js ] && wget https://unpkg.com/reveal.js@^4.6.0//plugin/search/search.js -O js/search.js
+[ ! -f js/notes.js ]  && wget https://unpkg.com/reveal.js@^4.6.0//plugin/notes/notes.js   -O js/notes.js
+[ ! -f js/reveal.js ] && wget https://unpkg.com/reveal.js@^4.6.0//dist/reveal.js		  -O js/reveal.js
+[ ! -f js/zoom.js ]   && wget https://unpkg.com/reveal.js@^4.6.0//plugin/zoom/zoom.js	  -O js/zoom.js
+
 
 # Check Parameter Empty
 [ -z "$1" ] && { echo -e "Some or all of the parameters are empty. \n\n Usage: $0 {{ name_of_file }}.md \n"; exit 1; }
@@ -24,7 +27,8 @@
 [ "${1: -5}" == ".html" ] || { echo "File is not a html file."; exit 1; }
 
 # Array of file names
-declare -a StringArray=("league.css" \
+declare -a StringArray=("moon.css" \
+                        "source-sans-pro.css" \
 						"reset.css" \
 						"reveal.css" \
 						"search.js" \
@@ -37,3 +41,4 @@ for i in "${StringArray[@]}"; do
 	newname=$(echo "$i" | cut -d "." -f 2)/"$i"
 	sed -i "s|https.*$i|$newname|g" "$1"
 done;
+
